9d5bc01c8c0725c2cd782d1ad08dbbab85bf0430
navakanth1 <navakanthreddy4445@gmail.com>
2022-03-30 12:56:53 +0530

    updated

--- test_amazon.py
+++ test_amazon.py
@@ -2,15 +2,15 @@ from selenium import webdriver
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 print("test case started")
+driver.maximize_window()
 driver.get("https://www.amazon.in/")
 driver.find_element_by_id("twotabsearchtextbox").send_keys("oneplus 9pro")
 time.sleep(1)
 driver.find_element_by_id("nav-search-submit-button").click()
 time.sleep(1)
-driver.find_element_by_partial_link_text("75,999").click()
+driver.find_element_by_partial_link_text("64,999").click()
 time.sleep(10)
 driver.close()
 print("test case created")
