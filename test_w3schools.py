d7bbaaeb7b637d58c7aa1553c9e6f61905dbd9ea
navakanth1 <navakanthreddy4445@gmail.com>
2022-03-30 12:06:35 +0530

    up

---
+++ test_w3schools.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+print("test case started")
+driver.maximize_window()
+driver.get("https://www.w3schools.com/")
+driver.find_element_by_link_text("Tutorials").click()
+time.sleep(1)
+driver.find_element_by_link_text("Python Tutorial").click()
+time.sleep(5)
+driver.close()
+print("test case completed")
